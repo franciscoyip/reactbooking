9532263a6fb82a56264eb71cd94badcb1f20724f
Francisco Yip <francisco.yip@gmail.com>
2016-11-13 17:53:55 -0800

    tweaking server

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 const Data = require('./serverDataController');
 var app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 
 // serve static assets normally
+app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/rooms', function (request, res){
